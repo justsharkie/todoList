e7b3924aa1c35dca177e4f025a0dfdc16c193688
Shayna <shaynawark@algonquinlive.com>
2017-06-24 19:34:20 -0400

    Functionality
    editing works!,
    testing the local storage

--- js/app.js
+++ js/app.js
@@ -1,21 +1,32 @@
 angular
     .module('myApp', [])
-    .controller('TodoCtrl', function($scope){
-        $scope.todos = [
-            {text: 'Create a To do!', date: 'Now', done: false},
+    .controller('TodoCtrl', function ($scope) {
+        $scope.saved = localStorage.getItem('todos');
+        $scope.todos = (localStorage.getItem('todos')!==null) ? JSON.parse($scope.saved) : [
+            {
+                text: 'Create a To do!',
+                date: 'Now',
+                done: false
+            },
         ];
+        localStorage.setItem('todos', JSON.stringify($scope.todos));
         $scope.completed = [
-            {text: 'Complete Task', date: 'Now', done:true},
-        ]
-        $scope.addTodo = function() {
-            $scope.todos.push({text:$scope.formTodoText, date:$scope.formTodoDate, done:false});
+
+        ];
+        $scope.addTodo = function () {
+            $scope.todos.push({
+                text: $scope.formTodoText,
+                date: $scope.formTodoDate,
+                done: false
+            });
             $scope.formTodoText = '';
             $scope.formTodoDate = '';
+            
         };
-        $scope.delete = function(){
+        $scope.delete = function () {
             $scope.todos.splice(this.$index, 1);
         };
-        $scope.completeItem = function(index) {
+        $scope.completeItem = function (index) {
             $scope.completed.push($scope.todos[index]);
             $scope.todos.splice(index, 1);
         };
